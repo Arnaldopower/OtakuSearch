4f0716b3f0b6e40358a1f4cafc3ee31083533ef2
TheSmuks <therealsmuks@gmail.com>
2024-03-08 10:48:13 +0100

    chore: cleanup

--- web/views.py
+++ web/views.py
@@ -1,38 +1,9 @@
 from django.shortcuts import render, redirect
 from django.views import View
-from django.contrib.auth import authenticate, login
-
-from web.forms import NameForm
-
-
-class MemberView(View):
-    def get(self, request):
-        if request.user.is_authenticated:
-            return render(request, 'members.html')
-        return redirect('home')
 
 
 class HomeView(View):
     def get(self, request):
-        if request.user.is_authenticated:
-            return redirect('members')
-        else:
-            form = NameForm()
-            return render(request, 'home.html', {"form": form})
-
-    def post(self, request):
-        form = NameForm(request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return redirect('members')
+        if not request.user.is_authenticated:
+            return redirect('accounts/login')
         return render(request, 'home.html')
-
-
-class GetNameView(View):
-    def get(self, request):
-        form = NameForm()
-        return render(request, "name.html", {"form": form})
