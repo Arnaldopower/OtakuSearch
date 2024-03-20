0fd782b73d4a25b2d50394679d2113e0d0c30e12
oscarvdcr <73657935+oscarvdcr@users.noreply.github.com>
2024-03-20 15:19:51 +0100

    Models Update: Anime and Status implementation

--- web/models.py
+++ web/models.py
@@ -6,29 +6,40 @@ from django.db import models
 # DON'T FORGET to run `python manage.py makemigrations` and `python manage.py migrate` after changing this file!!!
 
 
-class Manga(models.Model):
+class Manga(models.Model): #Matias
     pass
 
 
-class Author(models.Model):
+class Author(models.Model): #Angel
     pass
 
 
-class Status(models.Model):
+class Status(models.Model): #Oscar
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.id
+
+
+class Adaptations(models.Model): #Joel
     pass
 
+class Anime(models.Model): #Oscar
+    name = models.CharField(max_length=150)
+    seasons = models.IntegerField()
+    description = models.CharField(max_length=500)
+    cover = models.URLField()
+    rating = models.FloatField(min=0.0, max=1.0)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
 
-class Adaptations(models.Model):
+    def __str__(self):
+        return self.id
+
+class AnimeSeason(models.Model): #Arnau
     pass
 
-class Anime(models.Model):
+class Genre(models.Model): #Joel 
     pass
 
-class AnimeSeason(models.Model):
-    pass
-
-class Genre(models.Model):
-    pass
-
-class Studio(models.Model):
+class Studio(models.Model): #Arnau
     pass
