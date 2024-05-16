921c79706ecefdf4919ad9eedc5e990c5bb8e7e6
TheSmuks <therealsmuks@gmail.com>
2024-05-16 08:29:49 +0200

    feat: style comment test

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import SignUpView, LoginView, LogoutView, DeleteView
 
 urlpatterns = [
-    path("signup", SignUpView.as_view(), name="signup"),
-    path("login", LoginView.as_view(), name="login"),
-    path("logout", LogoutView.as_view(), name="logout"),
-    path("delete", DeleteView.as_view(), name='delete'),
+    path("signup/", SignUpView.as_view(), name="signup"),
+    path("login/", LoginView.as_view(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("delete/", DeleteView.as_view(), name='delete'),
 ]
